4422b5a7f2bc7fceb4a817946e65687fe15e8891
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-07-06 18:22:22 +0530

    Done the count digits code of step 1.4

--- Step-1_Learn the basics/Step 1.4/countDigits.cpp
+++ Step-1_Learn the basics/Step 1.4/countDigits.cpp
@@ -1,14 +1,38 @@
-#include<iostream>
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
 using namespace std;
-int main(){
-    int N=12;
-int cnt=0 , i=0 , num = N;
-    while(num){
-        i = num%10;
-        num/10;
-        if(N%i == 0)
-            cnt++;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int cnt=0 , num = N , i=0;
+        if(N == 0)
+            return 0;
+        while(num)
+        {
+            i = num%10;
+            if(i!=0 && N%i == 0)
+                cnt++;
+            num = num/10;
+        }
+        return cnt;
     }
-    cout<<cnt;
-return 0;
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
 }
+// } Driver Code Ends
